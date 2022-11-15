9cfdda323234e14b52ba0c0618001cc38be6c383
Bhakti0211 <bhakti.khara.sbgs@gmail.com>
2022-11-15 12:19:56 +0530

    added new project

---
+++ Calculator/Calc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calc
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    
+    public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
